a96905624adb5145011471f1241e047762450c68
Sebastian Gomez <js.gomez23@uniandes.edu.co>
2019-10-24 15:51:14 -0500

    Corrección 998B codeforces

--- Problems/CodeForces998B.java
+++ Problems/CodeForces998B.java
@@ -6,72 +6,45 @@ import java.util.ArrayList;
 public class CodeForces998B {
 	public static void main(String[] args) {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-			String[] st = br.readLine().split(" ");
-			int n = Integer.parseInt(st[0]);
-			int b = Integer.parseInt(st[1]);
-			int nums[] = new int[n];
-			st = br.readLine().split(" ");
-			for( int i = 0; i<n ; ++i){
-				nums[i] = Integer.parseInt(st[i]);
-			}
-			int[] pares = new int[n];
-			int[] impares = new int[n];
-			pares[0] = 0;
-			impares[0] = 0;
-			for(int i = 0 ; i< n ; ++i){
-				if(nums[i] % 2 == 0){
-					if(i == 0)
-						pares[i] = 1;
-					else{
-						pares[i] = pares[i-1]+1;
-						impares[i] = impares[i-1];
-					}
-						
-				}
-					
-				else{
-					if(i == 0){
-						impares[i] = 1;
-					}
-						
-					else{
-						impares[i] = impares[i-1]+1;
-						pares[i] = pares[i-1];
-					}
-						
-				}
-			}
 			
-			ArrayList<Integer> posiblesCortes = new ArrayList<>();
-			for(int i = 0; i < n-1; ++i){
-				if(pares[i] == impares[i])
-					posiblesCortes.add(i);
-			}
-			int[][] dp = new int[posiblesCortes.size()+1][b+1];
+			int n,coins;
+			String[] s = br.readLine().split(" ");
 			
-			for(int i = 0; i<b+1;++i){
-				dp[0][i] = 0;
+			n = Integer.parseInt(s[0]);
+			coins = Integer.parseInt(s[1]);
+			int[] numbers = new int[n];
+			s = br.readLine().split(" ");
+			for(int i = 0; i < n; ++i) {
+				numbers[i] = Integer.parseInt(s[i]);
 			}
-			for(int i = 1 ; i < posiblesCortes.size()+1; ++i){
-				for(int j = 0; j < b+1 ; ++j){
-					int  pos = posiblesCortes.get(i-1);
-					int ele1 = nums[pos];
-					int ele2 = nums[pos+1];
-					int peso = ele1 < ele2 ? ele2-ele1 : ele1-ele2;
-					if(j == 0){
-						dp[i][j] = 0;
-					}
-					else if (peso > j){
-						
+			int even = 0;
+			int odd = 0;
+			ArrayList<Integer> costOfCuts = new ArrayList<>();
+			for(int i = 0 ; i < n ; ++i) {
+				if(numbers[i] % 2 == 0 )
+					++even;
+				else
+					++odd;
+				if(i+1 < n && even ==  odd)
+					costOfCuts.add(Math.abs(numbers[i]-numbers[i+1]));
+			}
+			int [][] dp = new int[costOfCuts.size()+1][coins+1];
+			for(int j = 0; j < coins+1; ++j) {
+				dp[0][j]=0;
+			}
+			for(int i = 1; i < costOfCuts.size()+1; ++i) {
+				int cost = costOfCuts.get(i - 1);
+				for(int j = 0; j < coins+1; ++j) {
+					if(cost <= j) 
+						dp[i][j] = Math.max(dp[i-1][j-cost] + 1, dp[i-1][j]);
+					else
 						dp[i][j] = dp[i-1][j];
-					}
-					else if(peso  <= j){
-						
-						dp[i][j] = Math.max(dp[i-1][j-peso] +1, dp[i-1][j]);
-					}
+					
 				}
 			}
-			System.out.println(dp[posiblesCortes.size()][b]);
+
+				
+			System.out.println(dp[costOfCuts.size()][coins]);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
